c9393fa9553d0b8f7e05c8062333034787547e6a
rubegartor <rubegartor@gmail.com>
2018-05-09 22:05:10 +0200

    Remove task reminders_send & reminders command from help

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 from discord.ext import commands
-from utils import mysql
 import discord
 import time
 import asyncio
 import datetime
 import configparser
 
-cogs = ['cogs.core', 'cogs.overwatch', 'cogs.fun', 'cogs.moderation', 'cogs.memes', 'cogs.music', 'cogs.osu', 'cogs.reminders']
+cogs = ['cogs.core', 'cogs.overwatch', 'cogs.fun', 'cogs.moderation', 'cogs.memes', 'cogs.music', 'cogs.osu']
 
 class Bot():
   def __init__(self):
@@ -39,8 +38,11 @@ class Bot():
 
     @self.bot.event
     async def on_ready():
-      await self.bot.change_presence(game=discord.Game(name='!help • {} Guilds'.format(len(self.bot.servers))), status=discord.Status.online)
+      await self.bot.change_presence(status=discord.Status.online, game=discord.Game(name='!help • {} Guilds'.format(len(self.bot.servers))))
       print('[INFO] PeritaBOT Ready!')
+      print('-----------------------------------')
+      print('[INFO] discord.py version: {}'.format(discord.__version__))
+      print('[INFO] Bot ID: {}'.format(self.bot.user.id))
 
     @self.bot.event
     async def on_message(message):
@@ -50,7 +52,6 @@ class Bot():
         help_cmd = ':gear: __**LISTA DE COMANDOS: **__\n'
         help_cmd += '```ini\n{}8ball [question] - La bola mágica responde a tu pregunta\n'.format(self.prefix)
         help_cmd += '{}memes help - Más información sobre el grupo de comandos\n'.format(self.prefix)
-        help_cmd += '{}reminders help - Más información sobre el grupo de comandos\n'.format(self.prefix)
         help_cmd += '{}aww - Fotos de animalitos <3\n'.format(self.prefix)
         help_cmd += '{}avatar [usernames] - Obtiene la foto de perfil de los usuarios mencionado\n'.format(self.prefix)
         help_cmd += '{}ping - Muestra la latencia (ms) del bot\n'.format(self.prefix)
@@ -62,6 +63,7 @@ class Bot():
         help_cmd += '{}choose [options] - Elige una opcion entre todas\n'.format(self.prefix)
         help_cmd += '{}cat - Muestra una foto aleatoria de gatos\n'.format(self.prefix)
         help_cmd += '{}help - Muestra este mensaje\n'.format(self.prefix)
+        help_cmd += '{}spamvoice - Spamea el canal de voz en el que estes\n'.format(self.prefix)
         help_cmd += '{}info - Muestra información sobre el servidor y el Bot```\n'.format(self.prefix)
         help_cmd += ':musical_note: __**COMANDOS MUSIC BOT: **__\n'.format(self.prefix)
         help_cmd += '```ini\n{}summon - Une al bot a tu canal de voz\n'.format(self.prefix)
@@ -100,10 +102,10 @@ class Bot():
 
     @self.bot.event
     async def on_command_error(error, ctx):
-      if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+      if isinstance(error, commands.errors.CommandNotFound):
         await self.bot.send_typing(ctx.message.channel)
         await self.bot.send_message(ctx.message.channel, 'El comando `{}` no existe!'.format(ctx.message.content.split(' ')[0]))
-      elif isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+      elif isinstance(error, commands.errors.MissingRequiredArgument):
         await self.bot.send_typing(ctx.message.channel)
         await self.bot.send_message(ctx.message.channel, 'El comando `{}` requiere argumentos'.format(ctx.message.content.split(' ')[0]))
 
@@ -126,35 +128,10 @@ class Bot():
             now_done = 0
           await asyncio.sleep(5)
         except Exception as e:
-          print('async chrono_notifier: {}'.format(e))
-
-    async def send_reminder():
-      await self.bot.wait_until_ready()
-      while not self.bot.is_closed:
-        try:
-          id_match = []
-          for server in self.bot.servers:
-            for member in server.members:
-              if member.id not in id_match:
-                id_match.append(member.id)
-                mySQL = mysql.Connect()
-                data = mySQL.select('SELECT * FROM reminders WHERE date="{}" AND user_id="{}"'.format(datetime.datetime.now().strftime("%d-%m-%Y"), member.id))
-                if data != []:
-                  for reminder in data:
-                    e = discord.Embed(title = 'Recordatorio!', color = 0xf94343)
-                    e.add_field(name = reminder[4], value = reminder[3])
-                    e.set_thumbnail(url = 'https://i.imgur.com/e4AhEc2.png')
-                    await self.bot.send_message(member, embed = e)
-                    mySQL.delete('DELETE FROM reminders WHERE id="{}"'.format(reminder[0]))
-          await asyncio.sleep(60)
-        except Exception as e:
-          print('async send_reminder: {}'.format(e))
-          pass
+          print('async chrono_notifier: [{}] {}'.format(type(e).__name__, e))
 
     self.bot.loop.create_task(chrono_notifier(self.chrono_channel_id))
     print('[INFO] chrono_notifier task created')
-    self.bot.loop.create_task(send_reminder())
-    print('[INFO] send_reminder task created')
     self.bot.run(self.token)
 
 if __name__ == '__main__':
