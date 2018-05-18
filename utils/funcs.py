b15403005bf30bc7ba04a6d1284ee47752ef9e7e
rubegartor <rubegartor@gmail.com>
2018-05-18 15:29:32 +0200

    Remove class from funcs.py

--- utils/funcs.py
+++ utils/funcs.py
@@ -1,12 +1,11 @@
 import os
 
-class Funcs:
-  def readFile(self, file):
-    with open(file, encoding='utf-8') as _file:
-      return _file.read()
+def readFile(file):
+  with open(file, encoding='utf-8') as _file:
+    return _file.read()
 
-  def toList(self, array):
-    return [(x.split('_')[0], x) for x in array]
+def toList(array):
+  return [(x.split('_')[0], x) for x in array]
 
-  def toFiles(self, path):
-      return [x for x in os.listdir(path)]
+def toFiles(path):
+    return [x for x in os.listdir(path)]
